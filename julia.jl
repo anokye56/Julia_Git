47b209f6b16baeaf2be7260b3d43ce72bb89bc8b
Anokye <anokye561@outlook.com>
2024-03-13 16:41:27 +0800

    Revert "added translateSclPoint() function (2 methods)"
    This reverts commit de9fac4d0feb5d89c344cd4589721249a31f4960.

--- julia.jl
+++ julia.jl
@@ -16,15 +16,4 @@ begin
     function translatePoint(pt::Points, δx::Int64, δy::Int64)
         return (pt.x + δx, pt.y + δy)
     end
-
-    function translateSclPoint(pt::Points, δx::Int64, δy::Int64, ftr::Float64 = 0.2)
-        return ftr .* (pt.x + δx, pt.y + δy)
-    end
-
-    function translateSclPoint(pt::Points, δx::Float64, δy::Float64, ftr::Float64 = 0.2)
-        return ftr .* (pt.x + δx, pt.y + δy)
-    end
 end
-
-pp = Points(343, 232)
-translateSclPoint(pp, 4.6, 3.4)
