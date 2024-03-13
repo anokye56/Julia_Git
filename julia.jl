dfd97cccf88156366306503520d6a4d81f0993e5
Anokye <anokye561@outlook.com>
2024-03-13 16:13:06 +0800

    added translatePoint() function (2 methods)

--- julia.jl
+++ julia.jl
@@ -8,4 +8,12 @@ begin
     function scalePoints(pt::Points, ftr::Float64)
         return (pt.x * ftr, pt.y * ftr)
     end
+
+    function translatePoint(pt::Points, δx::Float64, δy::Float64)
+        return (pt.x + δx, pt.y + δy)
+    end
+
+    function translatePoint(pt::Points, δx::Int64, δy::Int64)
+        return (pt.x + δx, pt.y + δy)
+    end
 end
